f63e118be6540554840271004cc3cd51dfda259d
huangm9777 <huangm9777@uwec.edu>
2024-09-08 15:20:08 -0700

    created landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lab1 from './Labs/Lab1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -17,8 +18,51 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        {/* <Lab1 /> */}
+        <div>
+        
+          <h1>Mingfu Huang, CS5610.20596.202510</h1>
+            
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LAB ASSIGNMENTS
+            </a> 
+            <br/>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              KANBAS APPLICATION 
+            </a> 
+            <br/>
+            <a
+              className="App-link"
+              href="https://github.com/huangm9777/kanbas-react-web-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CODE REPOSITORY 
+            </a> 
+          
+          
+          <body>
+              <div >
+                  
+              </div>
+
+
+          </body>
+      </div>
       </header>
+
+
     </div>
   );
 }
