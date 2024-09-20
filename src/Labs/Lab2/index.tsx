fb8a9955bfab185639e6b3ce6ff9c89692007473
huangm9777 <huangm9777@uwec.edu>
2024-09-19 17:09:52 -0700

    finishing a1

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,18 @@
+import "./index.css";
+
 export default function Lab2() {
   return (
     <div>
       <h2>Lab 2</h2>
+      <p>Styled Component is imported</p>
+      <h2>Colors</h2>
+
+
+      <p className="styled-text-2">
+      I now have the ability to generate images and browse the web to provide real-time information, helping with tasks like staying updated on current events or creating visual content. Additionally, I can remember and tailor responses based on our past conversations, like your experiences and interests in Spring Cloud, cryptocurrency, and ML models. Anything new you'd like me to know or help you with?
+      </p>
+
+
     </div>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab1 from "./Lab1";
 export default function Labs() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Labs() {
       <h1>Labs</h1>
       <Routes>
         {/* <Route path="/" element={<Navigate to="Lab1" />} /> */}
+        <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
