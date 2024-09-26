e820a58bbdec336d552427f4af0e599988168f85
huangm9777 <huangm9777@uwec.edu>
2024-09-26 16:27:52 -0700

    alt prop

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -84,7 +84,7 @@ export default function Lab1() {
         <img id="wd-starship"
           width="400px"
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-          alt="image"
+          alt=""
         />
       </div>
       <div id="wd-forms">
